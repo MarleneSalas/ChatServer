bf59fcdce244753f6d6816aa0b9930beb561a17d
Marlene Abigail Salas Guerrero <marlene.salas2218@gmail.com>
2024-05-14 12:39:24 -0600

    Agregar archivos de proyecto.

---
+++ ChatClient/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChatClient
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            string url = "https://chat_itesrc.net/chat";
+            var _connection = new HubConnectionBuilder().WithUrl(url).WithAutomaticReconnect().Build();
+
+            _connection.On<string>
+        }
+    }
+}
---
+++ ChatServer/Services/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatServer.Services
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+
+        }
+    }
+}
