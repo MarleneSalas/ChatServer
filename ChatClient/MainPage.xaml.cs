93968c9c026bc79ea5dedb5b69003bb85831c091
Marlene Abigail Salas Guerrero <marlene.salas2218@gmail.com>
2024-05-14 12:53:17 -0600

    .

--- ChatClient/MainPage.xaml.cs
+++ ChatClient/MainPage.xaml.cs
@@ -4,14 +4,28 @@ namespace ChatClient
 {
     public partial class MainPage : ContentPage
     {
-        
+        private HubConnection _connection;
+
         public MainPage()
         {
-            InitializeComponent();
             string url = "https://chat_itesrc.net/chat";
-            var _connection = new HubConnectionBuilder().WithUrl(url).WithAutomaticReconnect().Build();
+            _connection = new HubConnectionBuilder().WithUrl(url).WithAutomaticReconnect().Build();
 
-            _connection.On<string>
+            _connection.On<string>("ReceiveMessage", message =>
+            {
+                lblHistory.Text += $"(message)<br/>";
+            });
+        }
+
+        private async void btnSend_Clicked(object sender, EventArgs e)
+        {
+            object a = txtMessage.Text;
+            await _connection.InvokeAsync("SendMessage", a);
+        }
+
+        private void ContentPage_Loaded(object sender, EventArgs e)
+        {
+            _connection.StartAsync();
         }
     }
 }
